3aaa346f2c4ca78bce17c739c3ff8b72d5ec5cff
WelbyC <welby.c@yahoo.com>
2018-03-08 10:51:27 -0800

    changing sketch

--- p5-blankExample/sketch.js
+++ p5-blankExample/sketch.js
@@ -1,17 +1,28 @@
 function setup() {
-  createCanvas(500, 500);
-
-  // set background color in grayscale:
-  background(150);
-
-  // draw ellipse
-  ellipseMode(CORNER);
-  ellipse(100, 100, 80, 80);
-
-  // draw rectangle
-  rect(100, 100, 80, 80)
+	createCanvas(500, 500);
+	// background is very light grey
+	background(230);
 }
 
 function draw() {
-  // no need to loop
+
+	// create bright green ellipse with white stroke
+	// fill is bright green
+	fill(0, 255, 0);
+	// stroke is white
+	stroke(255, 255, 255);
+	strokeWeight(1); // reset to default
+	ellipse(100, 100, 80, 80);
+
+	// make transparent rectangle
+	// a color fill with a fourth argument will define transparency
+	// fill has about 50% transparency
+	fill(255, 100, 50, 125);
+	noStroke();
+	rect(100, 100, 80, 80);
+
+	// stroke weight will set thickness of line
+	stroke(100, 200, mouseY, mouseX);
+	strokeWeight(5);
+	line(100, 100, mouseX, mouseY);
 }
