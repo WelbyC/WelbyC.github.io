eb30207bb1312e2b594afe1f5b723901f20a80ce
WelbyC <welby.c@yahoo.com>
2018-03-20 10:21:16 -0700

    Commiting to change in sketch

--- p5-blankExample/sketch.js
+++ p5-blankExample/sketch.js
@@ -1,29 +1,15 @@
 function setup() {
 	createCanvas(500, 500);
-	// background is very light grey
-	background(230);
+	background(200);
 }
 
 function draw() {
-
-	// create bright green ellipse with white stroke
-	// fill is bright green
-	fill(0, 0, 0);
-	// stroke is white
-	stroke('red');
-	strokeWeight(1); // reset to default
-	ellipseMode(CORNER);
-	ellipse(100, 100, 80, 80);
-
-	// make transparent rectangle
-	// a color fill with a fourth argument will define transparency
-	// fill has about 50% transparency
-	fill(255, 100, 50, 125);
-	noStroke();
-	rect(100, 100, 80, 80);
-
-	// stroke weight will set thickness of line
-	stroke(0, 0, mouseY, mouseX);
-	strokeWeight(5);
-	line(100, 100, mouseX, mouseY);
+	if (mouseIsPressed) {
+		fill(10, 100, 200);
+		ellipse(mouseX, mouseY, 80, 80);
+	} else {
+		fill(255, 100, 50);
+		rectMode(CENTER);
+		rect(mouseX, mouseY, 80, 80);
+	}
 }
