0c80f25adbb3073905ae51f64d8be4724381853e
unknown <Welby.c@yahoo.com>
2019-01-12 03:10:19 -0800

    Changes on 1/12/2019 updating website

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-var MARGIN = 50;
+var MARGIN = 44;
 let virus;
 let ground;
 let platform3;
@@ -10,28 +10,29 @@ var entered = 0;
 
 function setup(){
 
-	createCanvas(windowWidth-40, windowHeight-100);
-	
+	createCanvas(windowWidth, windowHeight-100);
+    background("#f5f5f5");
+    document.body.style.backgroundColor = "#001c49";
 	let numberChoices = ['laugh','mystery','ninja'];
 	let ohno = ['laugh','mystery','ninja','virus'];
-
+    
 
   
-
+    //ground and platform sprites aka lines with color
 	ground = createSprite(width / 2, height - 50, width +100, 2);
     platform3 = createSprite(width / 2 +width/2.6, height - height/4.2, width/11, 10);
     platform2 = createSprite(width / 2, height - height/4.2, width/11, 10);
     platform1 = createSprite(width / 2 -width/2.6, height - height/4.2, width/11, 10);
     ground.shapeColor = color(0);
-
-    door = createSprite(width/2,height - height/2.8 ,250,150);
+    //door sprite leading to github
+    door = createSprite(width/2,height - height/2.8 ,width/11,width/11);
     door.addAnimation('closedDoor','sprite0.png');
     door.addAnimation('openDoor','sprite1.png');
-    door.setCollider("circle",0,0,50,50);
+    door.setCollider("rectangle",0,0,50,50);
     door.scale = 0.8;
 
-
-	virus = createSprite(width / 2 -width/2.6,0,150,100);
+    //player sprite that can be a variety of things
+	virus = createSprite(width / 2 -width/2.6,0,width/11,width/11);
 	virus.addAnimation("virus","portfolio/3A/virus_0.png","portfolio/3A/virus_1.png","portfolio/3A/virus_1.png");	
 	virus.addAnimation('laugh','portfolio/3A/laugh1.png','portfolio/3A/laugh0.png','portfolio/3A/laugh0.png'); 
 	virus.addAnimation('mystery','portfolio/3A/mystery1.png','portfolio/3A/mystery0.png','portfolio/3A/mystery0.png');
@@ -49,7 +50,7 @@ function setup(){
 
 	virus.setCollider("circle",0,0,50);
 
-
+    //this hole sprite leads to art portfolio
 	hole = createSprite(width - width/8.6,height - height/3,200,150);
 	hole.addAnimation('hole','portfolio/3A/hole0.png','portfolio/3A/hole1.png','portfolio/3A/hole2.png','portfolio/3A/hole3.png');
     hole.setCollider("circle",0,0,0,25);
@@ -66,15 +67,15 @@ function setup(){
 
 function draw(){
 background(255);
-	textSize(45);
+	textSize(width/50);
 	text("W,A,S,D to move", width/2,height-height/1.1);
-    textSize(20);
+    textSize(width/80);
 	text("Infinite Jumps", width/2,height-height/1.15);
-    textSize(20);
+    textSize(width/80);
     text("GitHub", width/2.07,height - height/2);
-    textSize(20);
+    textSize(width/80);
     text("Art Porfolio", width - width/7,height-height/2);
-	textSize(20);
+	textSize(width/80);
 
     //eye player controls
     //left, right
