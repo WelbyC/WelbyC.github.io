a613462f8600f3d6bea7005f54e6aaf4200eca71
unknown <Welby.c@yahoo.com>
2020-09-29 15:09:12 -0700

    Randomizer update

--- art101/randomizer/sketch.js
+++ art101/randomizer/sketch.js
@@ -1,39 +1,31 @@
 let button;
-let fates = [
-  "Celebrity",
-  "Dead Early",
-  "Teacher",
-  "Hater",
-  "Villain",
-  "Hero",
-  "Average",
-  "Janitor",
-  "Sales Clerk",
-  "Gamer",
-  "Gave up",
-  "President",
-  "Professional Couch Warmer",
-  "Successful",
-];
+let fates = ["Celebrity","Dead Early","Teacher","Hater", "Villain", "Hero", "Average", "Janitor", "Sales Clerk", "Gamer", "Gave up","President", "Professional Couch Warmer", "Successful"];
 let fate = fates[1];
 
 function setup() {
   createCanvas(800, 800);
   background(255);
-  button = createButton("Life Button");
+  button = createButton('Life Button');
   button.position(19, 19);
+  
+
 }
 
 function draw() {
   button.mousePressed(randomLife);
+  
+
+  
 }
 
 function randomLife() {
   textSize(32);
-  text(fates[int(random(0, 14))], random(50, 750), random(50, 750));
+  text(fates[int(random(0,14))], random(50, 750), random(50, 750));
   fill(0, 102, 153);
-  text(fates[int(random(0, 14))], random(50, 750), random(50, 750));
+  text(fates[int(random(0,14))], random(50, 750), random(50, 750));
   fill(0, 102, 153);
-  text(fates[int(random(0, 14))], random(50, 750), random(50, 750));
+  text(fates[int(random(0,14))], random(50, 750), random(50, 750));
   fill(0, 102, 153);
+  
+
 }
