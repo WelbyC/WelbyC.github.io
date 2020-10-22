a9294afda9f31128692fbc67eb0e81218f4fe54a
unknown <Welby.c@yahoo.com>
2020-10-22 14:27:54 -0700

    fading noise function

--- art101/drawing/sketch.js
+++ art101/drawing/sketch.js
@@ -1,13 +1,29 @@
 let array = [];
 let ellipses = false
 let lines = false
+let noiseOffset = 0.0
+let strokeWidth = 5
+
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   
   background(220, 50, 25)
+
+  
 }
 
 function draw() {
+  background(220, 50, 25, 10)
+  strokeWeight(strokeWidth)
+
+  noiseOffset += 0.05
+  strokeWidth = noise(noiseOffset) * 100
+
+  
+
+
+
   if(mouseIsPressed){
     //stroke(map(mouseX, 0, 600, 0, 255, true))
     //line(width - mouseX,height - mouseY,width - pmouseX, height - pmouseY)
@@ -44,6 +60,9 @@ function keyTyped(){
   if(key === "s"){
     // save this image
     saveCanvas("fileName", "png")
+  } else if (key === "c"){
+
+  clear();
   }
 
   return false;
