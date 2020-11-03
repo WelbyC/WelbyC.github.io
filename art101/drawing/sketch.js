b4143785a4675bb612e0cbeea7557de8ec0f327f
unknown <Welby.c@yahoo.com>
2020-11-03 14:16:06 -0800

    pushing drawing machines

--- art101/drawing/sketch.js
+++ art101/drawing/sketch.js
@@ -1,59 +1,103 @@
 let array = [];
 let ellipses = false
 let lines = false
+let shrink = false
+let grow = false
+let ghost = false
 let noiseOffset = 0.0
 let strokeWidth = 5
 let numCells
-
+let n = 40
+let ghosts = []
+let rghost;
 
 function setup() {
-  createCanvas(600, 600);
+  createCanvas(windowWidth , windowHeight);
   
-  background(220, 50, 25)
-
-  
+  background(175,238,238)
+  ghost1 = loadImage('Ghost-1.png')
+  ghost2 = loadImage('Ghost-2.png')
+  ghosts = [ghost1,ghost2]
 }
 
 function draw() {
   //background(220, 50, 25, 10)
-//  strokeWeight(strokeWidth)
-
-//  noiseOffset += 0.05
-// strokeWidth = noise(noiseOffset) * 100
+  //strokeWeight(strokeWidth)
+  //noiseOffset += 0.05
+  //strokeWidth = noise(noiseOffset) * 100
 
   
 
 
-
+  rghost = random(ghosts)
   if(mouseIsPressed){
+      if(ghost){
+        image(rghost, mouseX-25, mouseY-25, 50, 50)
+    
+  }
     //stroke(map(mouseX, 0, 600, 0, 255, true))
     //line(width - mouseX,height - mouseY,width - pmouseX, height - pmouseY)
     //line(mouseX,mouseY,pmouseX,pmouseY)
-    array.push([mouseX, mouseY])
+    //array.push([mouseX, mouseY])
   }
   if(lines){
+  strokeWeight(strokeWidth)
+  noiseOffset += 0.05
+  strokeWidth = noise(noiseOffset) * 100
+  
+  //line(width - mouseX,height - mouseY,width - pmouseX, height - pmouseY)
+  line(mouseX,mouseY,pmouseX,pmouseY)
+    array.push([mouseX, mouseY])
+/*    
     for(let i = 0; i< array.length-1;i++){
       line(array[i][0],array[i][1],array[i+1][0],array[i+1][1])
     }
+*/
   }
   if(ellipses){
-    for(let i = 0; i < array.length-1; i++){
-      ellipse(mouseX, mouseY, 20, 20)
-    }
+    strokeWeight(1)
+      ellipse(width - mouseX,height - mouseY, 20, 20)
+    
   }
+  if(shrink){
+    
+    strokeWeight(n)
+    line(mouseX,mouseY,pmouseX,pmouseY)
+    n -= 0.5
+  }
+  if(grow){
+    
+    strokeWeight(n)
+    line(mouseX,mouseY,pmouseX,pmouseY)
+    n += 0.5
+  }
+
   if(keyIsDown(49)){
     lines = true
   }
   if(keyIsDown(50)){
     ellipses = true
   }
+    if(keyIsDown(51)){
+    shrink = true
+  }
+    if(keyIsDown(52)){
+    grow = true
+  }
+    if(keyIsDown(53)){
+    ghost = true
+  }
   if(!keyIsPressed){
     lines = false
     ellipses = false
-  //  background(220, 50, 25)
+    shrink = false
+    grow = false
+    ghost = false
+    n = 40
+    //background(220, 50, 25)
   }
  
-  drawGrid()
+ console.log(rghost)
   }
 
 
@@ -69,18 +113,3 @@ function keyTyped(){
   return false;
 }
 
-function drawGrid(){
-  numCells = 20
-  fillColor = 255
-  for(let i = 0; i <= width; i+=width / numCells){
-    for(let j =0; j <= height; j +=height / numCells){
-      if(fillColor ===255){
-        fillColor = 200
-      } else{
-        fillColor = 255
-      }
-      fill(fillColor)
-      rect(i, j, width / numCells, height / numCells)
-    }
-  }
-}
