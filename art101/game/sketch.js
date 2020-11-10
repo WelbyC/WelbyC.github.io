a26af6a21b2ebdea4b12f1e55d910de867986b55
unknown <Welby.c@yahoo.com>
2020-11-10 15:49:14 -0800

    finished fixing bugs

--- art101/game/sketch.js
+++ art101/game/sketch.js
@@ -1,5 +1,6 @@
 let state = "title"
 let cnv
+let points = 0
 
 function setup() {
   cnv = createCanvas(600, 600);
@@ -7,20 +8,26 @@ function setup() {
 }
 
 function draw() {
-  
-  if(state==="title"){
-    title();
-    cnv.mouseClicked(titleMouseClicked)
-  }
-  else if(state === "level1"){
-    level1();
-    
-  }
+  switch(state){
+    case "title":
+      title();
+      cnv.mouseClicked(titleMouseClicked)
+      break
+    case "level1":
+      level1()
+      cnv.mouseClicked(level1MouseClicked)
+      break
+    case "win":
+      win()
+      cnv.mouseClicked(youWinMouseClicked)
+      break
+    default:
+      break
+         }
+
 }
 
-function mousePressed(){
-  state = "level1"
-}
+
 
 
 function title(){
@@ -39,10 +46,26 @@ function titleMouseClicked(){
 
 function level1(){
   background(50, 150, 200)
+  text("click for points", 250, 400)
   
 }
-
-function level1MouseClicked(){
+function win(){
+  background(255,100,200)
+  textSize(82)
+  stroke(255)
+  text("YOU WIN", 100, 100)
   
+  textSize(20)
+  text("Click to restart", 250,300) 
+}
+function youWinMouseClicked(){
+  state = "title"
+  points = 0
+}
+function level1MouseClicked(){
+  points++;
+  if(points >=10){
+    state = "win"
+  }
 }
 
