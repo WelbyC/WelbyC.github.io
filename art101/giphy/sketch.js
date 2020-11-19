0581d686e4de8f2bb2f920936c4526a1042defbc
unknown <Welby.c@yahoo.com>
2020-11-19 14:43:14 -0800

    gifs

---
+++ art101/giphy/sketch.js
@@ -0,0 +1,29 @@
+
+
+var api = "http://api.giphy.com/v1/gifs/search?";
+
+var apiKey = "&api_key=dc6zaTOxFJmzC";
+
+var query = "&q=rainbow";
+
+
+
+
+function setup() {
+  noCanvas();
+  // the entire url from variables above
+  var url = api + apiKey + query;
+  //gets us the json with the gifs
+  loadJSON(url, gotData);
+}
+
+function gotData(giphy) {
+  //the loop goes through the JSON andlinks all the gifs in the section
+  for(var i = 0; i<giphy.data.length;i++){
+  createImg(giphy.data[i].images.original.url);
+  }
+}
+
+function draw(){
+
+}
